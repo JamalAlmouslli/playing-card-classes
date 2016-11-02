699f24fcc11c68207735245ce516ad0fa726cc7e
Jamal <JamalAlmouslli@users.noreply.github.com>
2016-11-02 12:59:21 -0500

    Update Card.hpp

--- Card.hpp
+++ Card.hpp
@@ -8,44 +8,44 @@
 
 enum class Rank : char
 {
-	Ace,
-	Two,
-	Three,
-	Four,
-	Five,
-	Six,
-	Seven,
-	Eight,
-	Nine,
-	Ten,
-	Jack,
-	Queen,
-	King
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
 };
 
 enum class Suit : char
 {
-	Clubs,
-	Diamonds,
-	Hearts,
-	Spades
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades
 };
 
 class Card
 {
 private:
-	static const boost::unordered_map<Rank, char> ranks;
-	static const boost::unordered_map<Suit, char> suits;
+    static const boost::unordered_map<Rank, char> ranks;
+    static const boost::unordered_map<Suit, char> suits;
 
-	Rank r;
-	Suit s;
+    Rank r;
+    Suit s;
 
 public:
-	Card() = delete;
-	Card(Rank rank, Suit suit);
-	const std::string to_string() const;
-	const Rank& rank() const noexcept;
-	const Suit& suit() const noexcept;
+    Card() = delete;
+    Card(Rank rank, Suit suit);
+    const std::string to_string() const;
+    const Rank& rank() const noexcept;
+    const Suit& suit() const noexcept;
 };
 
 std::ostream& operator<<(std::ostream& out, Card const& obj);
