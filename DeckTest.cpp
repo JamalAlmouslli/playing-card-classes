31ce7c3c066dc6e936ebd63f1e2f05c6e0226eaf
Jamal <JamalAlmouslli@users.noreply.github.com>
2016-11-02 12:57:13 -0500

    Update DeckTest.cpp

--- DeckTest.cpp
+++ DeckTest.cpp
@@ -4,23 +4,23 @@
 
 int main()
 {
-	Deck<Card> deck;
+    Deck<Card> deck;
 
-	for (Rank rank = Rank::Ace; rank <= Rank::King; ++rank)
-	{
-		for (Suit suit = Suit::Clubs; suit <= Suit::Spades; ++suit)
-		{
-			deck.add(Card(rank, suit));
-		}
-	}
+    for (Rank rank = Rank::Ace; rank <= Rank::King; ++rank)
+    {
+        for (Suit suit = Suit::Clubs; suit <= Suit::Spades; ++suit)
+        {
+            deck.add(Card(rank, suit));
+        }
+    }
 
-	deck.shuffle();
+    deck.shuffle();
 
-	while (!deck.empty())
-	{
-		std::cout << deck.draw() << "\n";
-	}
+    while (!deck.empty())
+    {
+        std::cout << deck.draw() << "\n";
+    }
 
-	std::cout << "\n\n";
-	std::cin.get();
+    std::cout << "\n\n";
+    std::cin.get();
 }
