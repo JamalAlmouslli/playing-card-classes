04cc8a0e96b0591902106193cb186fa249014cdf
Jamal Almouslli <jalmouslli@users.noreply.github.com>
2017-07-30 21:07:01 -0500

    Update DeckTest.cpp

--- DeckTest.cpp
+++ DeckTest.cpp
@@ -5,8 +5,8 @@
 int main()
 {
     Deck<Card> deck;
-
-	// add standard 52 cards to deck
+        
+    // add standard 52 cards to deck
     for (Rank rank = Rank::Ace; rank <= Rank::King; ++rank)
     {
         for (Suit suit = Suit::Clubs; suit <= Suit::Spades; ++suit)
@@ -14,8 +14,8 @@ int main()
             deck.add(Card(rank, suit));
         }
     }
-	
-	// shuffle, draw, and print each card
+        
+    // shuffle, draw, and print each card
     deck.shuffle();
 	
     while (!deck.empty())
